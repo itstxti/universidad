f8e3b993ecce116ac7d0d73e0d461d2ce788df07
Tati <tgarcia797@yahoo.com>
2023-06-09 18:07:50 +0200

    added new exercises

--- Año 1 (2022 - 2023)/Python/Cuaderno 8 - Recursividad/Ejercicio - 09.py
+++ Año 1 (2022 - 2023)/Python/Cuaderno 8 - Recursividad/Ejercicio - 09.py
@@ -1,1 +1,13 @@
-""" 8. Realiza una función recursiva que, dado un número entero, cuente su número de dígitos."""
+""" 9. Realiza una función recursiva que, dado un número entero, cuente su número de dígitos."""
+
+def contar_digitos(n):
+    if len(n) == 1:
+        return 1
+    else:
+        return 1 + contar_digitos(n[1:len(n)])
+    
+try:
+    n = int(input("Introduce un número: ")) # solo acepta numeros
+    print(contar_digitos(str(n))) # vuele a transformarlo en str
+except: print('ERROR, el número introducido no es válido.')
+
