fc68589f002f110919be549934654d8fde168722
Tati <tgarcia797@yahoo.com>
2023-06-09 15:10:47 +0200

    added new excercises

--- Año 1 (2022 - 2023)/Python/Cuaderno 8 - Recursividad/Ejercicio - 07.py
+++ Año 1 (2022 - 2023)/Python/Cuaderno 8 - Recursividad/Ejercicio - 07.py
@@ -1,2 +1,14 @@
 """ 4. Programa, haciendo uso de la recursividad, una función en Python
 que permita obtener el término de orden n de la sucesión de Fibonacci."""
+
+def sucesion_fibonacci(n):
+    if n == 0 or n == 1:
+        return 1
+    else:
+        return sucesion_fibonacci(n - 2) + sucesion_fibonacci(n - 1)
+
+          
+
+n = int(input('Introduce el termino que quieres de la sucesión de Fibonacci: '))
+print(sucesion_fibonacci(n)) # Pimer termino = 0.
+
