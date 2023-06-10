aabf3d0cc5674b6ee5771e7551d56602ea2d5e1e
Tati <tgarcia797@yahoo.com>
2023-06-10 15:07:18 +0200

    added new excercises

---
+++ Año 1 (2022 - 2023)/Python/Cuaderno 8 - Recursividad/Ejercicio - 15.py
@@ -0,0 +1,8 @@
+""" 15.	Las torres de Hanoi es un rompecabezas inventado por el matemático francés Édouard Lucas en 1883.
+
+Se dispone de 3 soportes y n discos de diferente radio.
+Inicialmente los discos están apilados en orden creciente en el soporte O (origen).
+Pasarlos al soporte D(destino), quedando en el mismo orden en que están actualmente.
+El soporte T(temporal) puede servir de auxiliar. Sólo se puede mover un disco cada vez.
+En cualquier soporte, en todo momento, cada disco debe tener un radio menor que el que tiene debajo."""
+
