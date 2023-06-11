910f3988dcf275f959ceaa76179acfba5490d4cd
Tati <tgarcia797@yahoo.com>
2023-06-11 22:53:41 +0200

    add new exercises

--- Año 1 (2022 - 2023)/Python/Bucles con condicion/Ejercicio - 05.py
+++ Año 1 (2022 - 2023)/Python/Bucles con condicion/Ejercicio - 05.py
@@ -8,11 +8,11 @@ Haz un subprograma que determine si hay unanimidad en cambiarlo."""
 
 quieres_cambiar= (True, True, True, True)
 
-def todos_unanimidad(lista):
+def unanimidad(lista):
     
     for i in range(0, len(lista)-1):
         if lista[i] == False:
             return False
     else: return True
 
-print(todos_unanimidad(quieres_cambiar))
+print(unanimidad(quieres_cambiar))
